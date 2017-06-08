1f6764cc87f0c26a89c3fb8d88520c8aa5853f13
Lubo <teaatsix@gmail.com>
2017-06-08 22:45:13 +0200

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,48 @@
+# math symbols:
+# addition +
+# subtraction -
+# division / (slash)
+# multiplication * (asterisk)
+# modulus (remainder) % (operator returns the remainder of the division)
+# comparison: less-than <
+# comparison: greater-than >
+# comparison: less-than-equal <=
+# comparison: greater-than-equal >=
+
+# What is th RRRFe order of operations?
+# In the United States we use an acronym called PEMDAS which stands for:
+# PEMDAS
+# Parentheses Exponents Multiplication Division Addition Subtraction. 
+# That's the order Python follows as well.
+
+print 5/2 #result 2 
+print 5.0/2 #result 2.5
+print 10/2*3 #result 15
+print 10/(2*3) #result 1, only integer
+print 10.0/(2*3) # result 1.6666666666667
+
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30.0 / 6
+print "Roosters", 100 - 25.0 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4.0 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# comparison (>,<,>=,<=) returns True or False values
+print 3 + 2 < 5 - 7 #False
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that`s why it`s False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2 #True
+print "Is it greater or equal?", 5 >= -2 #True
+print "Is it less or equal?", 5 <= -2 #False
+
---
+++ ex5.py
@@ -0,0 +1,25 @@
+#Variables continued...
+
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+# % (modulo) + letter -> are "formatters"
+# They tell Python to take the variable on the right and put it in to replace the %s with its value.
+# many letters in usage, check: https://docs.python.org/2.4/lib/typesseq-strings.html
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes,my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+	my_age, my_height, my_weight, my_age + my_height + my_weight)
+	
