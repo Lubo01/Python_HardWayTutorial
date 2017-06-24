d6c02e93bc8fd9211d05c0d4360368bc8e4cb104
Lubo <teaatsix@gmail.com>
2017-06-24 23:58:55 +0200

    exercise done

--- ex2.py
+++ ex2.py
@@ -1,7 +1,7 @@
 # Ex2 - Comments   
 
 # A comment, this is so you can read your program later.
-# Anything after the # is ignored by python.
+# Anything after the # is ignored by python. 
 
 print "I could have code like this." # and the comment after is ignored
 
