30610f0b5ee3dfe4bb861d8cc6233fe85d1348ea
Lubo <teaatsix@gmail.com>
2018-03-02 00:07:05 +0100

    project succesful

---
+++ web_form/bin/app.py
@@ -0,0 +1,24 @@
+#ex51 Getting Input from a Browser
+#Web form
+
+import web
+
+urls = (
+	'/hello', 'Index'
+)
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/')
+
+class Index(object):
+	def GET(self):
+		return render.hello_form()
+		
+	def POST(self):
+		form = web.input (name = "Nobody", greet = "Hello")
+		greeting = "Hello, %s, %s" % (form.greet, form.name)
+		return render.index(greeting = greeting)
+				
+if __name__ == "__main__":
+	app.run()
---
+++ web_form/bin/form_test.py
@@ -0,0 +1,24 @@
+#ex51 Getting Input from a Browser
+#Web form
+
+import web
+
+urls = (
+	'/hello', 'Index'
+)
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/')
+
+class Index(object):
+	def GET(self):
+		form = web.input (name = "Nobody", greet = None)
+		if form.greet:
+			greeting = "Hello, %s, %s" % (form.greet, form.name)
+			return render.index(greeting = greeting)
+		else:
+			return "ERROR: greet is required."
+		
+if __name__ == "__main__":
+	app.run()
